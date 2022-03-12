7976f84a98694d04a77c94e6e2d268baab7fd9c7
Victor0596647 <85453971+Victor0596647@users.noreply.github.com>
2022-03-12 06:41:46 -0800

    added getters and some setters

--- player.py
+++ player.py
@@ -1,19 +1,29 @@
 class Player:
     def __init__(self):
         # Variables
-        self.__player_name = ''
+        self.__player_name = 'null'
         self.__player_market = ['']
         self.__player_cash = 0
-        self.__player_company = ''
+        self.__player_company = 'null'
 
     def create_player(self, name, company, cash):
         self.__player_name = name
         self.__player_company = company
-        self.__player_cash = cash
+        self.__player_cash = int(cash)
 
     def create_market(self, market_name):
-        self.__player_market[0] = market_name
+        self.player_market.append(market_name)
 
     # Getters
     def get_player_name(self):
-        return self.__player_name
+        return '{}'.format(self.__player_name)
+
+    def get_company(self):
+        return '{}'.format(self.__player_company)
+
+    # Setters
+    def set_player_name(self, name):
+        self.__player_name = name
+
+    def set_company(self, company):
+        self.__player_company = company
